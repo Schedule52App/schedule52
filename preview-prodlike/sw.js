4b622ba56627afe244e029187a058fd803de9fd1
Glenn Wilbanks <glenn@wilbanksplumbing.com>
2026-05-19 14:58:55 +0000

    preview-prodlike: deploy floating-panel light mode (index-CER4e7a0.js / index-usV4MeFn.css), SW wc-v47

--- preview-prodlike/sw.js
+++ preview-prodlike/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wc-v46";
+const CACHE = "wc-v47";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
