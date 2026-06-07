5e6ad9e72ff33ebfb987aea52ab69876dc66de51
Glenn Wilbanks <gwillma@gmail.com>
2026-06-07 01:21:27 +0000

    By Tech view: block clicking past time slots for today + shade past region (wc-v170)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// cache-bust: 20260606-2342 wc-v169 tech-color-swimlanes
-const CACHE = "wc-v169";
+// cache-bust: 20260606-2342 wc-v170 tech-color-swimlanes
+const CACHE = "wc-v170";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
