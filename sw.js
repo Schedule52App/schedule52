441a5486a123e24532a95771b3dc270a00f46bf4
Glenn Wilbanks <gwillma@gmail.com>
2026-06-07 02:05:34 +0000

    wc-v175: rename calendar View toggle label 'By Tech' -> 'Tech'

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// cache-bust: 20260606-2342 wc-v174 tech-color-swimlanes
-const CACHE = "wc-v174";
+// cache-bust: 20260606-2342 wc-v175 tech-color-swimlanes
+const CACHE = "wc-v175";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
