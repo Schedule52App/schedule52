9fe7213f8a9ef9af9089479a98df6d2595540a81
Glenn Wilbanks <gwillma@gmail.com>
2026-06-07 23:05:25 +0000

    Deploy wc-v192: 6-tab Settings page (new JS+CSS, SW/cache bump)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// cache-bust: 20260606-2342 wc-v191 tech-color-swimlanes
-const CACHE = "wc-v191";
+// cache-bust: 20260606-2342 wc-v192 tech-color-swimlanes
+const CACHE = "wc-v192";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
