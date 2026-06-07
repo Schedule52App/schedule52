2433fcd2150664ae1d3a2ede5e44e155bd6573f5
Glenn Wilbanks <gwillma@gmail.com>
2026-06-07 01:09:45 +0000

    By Tech view: use pointer (hand) cursor on open slots for app consistency (wc-v167)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// cache-bust: 20260606-2342 wc-v166 tech-color-swimlanes
-const CACHE = "wc-v166";
+// cache-bust: 20260606-2342 wc-v167 tech-color-swimlanes
+const CACHE = "wc-v167";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
