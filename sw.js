7bea7932f536303df1b3743bc954c4fb65d0a861
Glenn Wilbanks <gwillma@gmail.com>
2026-06-08 02:17:58 +0000

    wc-v202: Users in Users & Roles tab (deploy)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// cache-bust: 20260606-2342 wc-v201 tech-color-swimlanes
-const CACHE = "wc-v201";
+// cache-bust: 20260606-2342 wc-v202 tech-color-swimlanes
+const CACHE = "wc-v202";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
