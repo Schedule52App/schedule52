5f960672ed8a8db2ee918c9cc12ebbc46de8211b
Glenn Wilbanks <gwillma@gmail.com>
2026-06-08 01:05:56 +0000

    wc-v196: deploy compacted sidebar (fits short viewports) to staging

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// cache-bust: 20260606-2342 wc-v195 tech-color-swimlanes
-const CACHE = "wc-v195";
+// cache-bust: 20260606-2342 wc-v196 tech-color-swimlanes
+const CACHE = "wc-v196";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
